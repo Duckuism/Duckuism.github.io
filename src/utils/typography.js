f825eb0f3cec8b1c6195f7c9a36ffe772e362fea
Duckuism <rnejrl123456@gmail.com>
2019-06-16 00:09:08 +0900

    Revert "typography theme 삭제"
    This reverts commit 0341fe5cadeb644df3a16dbc285a44dffc71bb40.

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,17 @@
 import Typography from "typography"
+import Wordpress2016 from "typography-theme-wordpress-2016"
 
-// Did not use a theme here. If you want to use theme, see https://kyleamathews.github.io/typography.js/.
-const typography = new Typography()
+Wordpress2016.overrideThemeStyles = () => {
+  return {
+    "a.gatsby-resp-image-link": {
+      boxShadow: `none`,
+    },
+  }
+}
+
+delete Wordpress2016.googleFonts
+
+const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
